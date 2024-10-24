e2707ebd01191282ca7081a4e9bc565e37d75589
Yannick Spies <yannickspies91@gmail.com>
2024-10-24 23:26:48 +0200

    added keymaps and options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,30 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+-- Line numbers and indentation
+opt.relativenumber = true -- Relative line numbers
+opt.scrolloff = 8 -- Keep 8 lines above/below cursor when scrolling
+opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
+opt.expandtab = true -- Use spaces instead of tabs
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.shiftwidth = 2 -- Size of an indent
+opt.smartindent = true -- Insert indents automatically
+
+-- Search
+opt.ignorecase = true -- Ignore case in search patterns
+opt.smartcase = true -- Override ignorecase if search contains uppercase
+
+-- Appearance
+opt.termguicolors = true -- True color support
+opt.wrap = false -- Disable line wrap
+opt.cursorline = true -- Highlight current line
+opt.signcolumn = "yes" -- Always show sign column
+
+-- Behavior
+opt.undofile = true -- Persistent undo history
+opt.updatetime = 250 -- Faster completion
+opt.timeout = true -- Enable timeout for mappings
+opt.timeoutlen = 300 -- Time to wait for mapped sequence to complete
+opt.clipboard = "unnamedplus" -- Use system clipboard
