7b1b6909474cfd1bf3c34257a21b5d824a0b553d
Yannick Spies <yannickspies91@gmail.com>
2024-11-16 01:51:38 +0100

    select all added and old keybinds removed

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -9,46 +9,20 @@ map("n", "<leader>tfo", ":TSToolsOrganizeImports<CR>", { desc = "Organize Import
 map("n", "<leader>tfr", ":TSToolsRenameFile<CR>", { desc = "Rename File" })
 map("n", "<leader>tfs", ":TSToolsSortImports<CR>", { desc = "Sort Imports" })
 
--- Better window navigation
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
-
--- Resize with arrows
-map("n", "<C-Up>", ":resize -2<CR>", { desc = "Decrease window height" })
-map("n", "<C-Down>", ":resize +2<CR>", { desc = "Increase window height" })
-map("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
-map("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
-
--- Move text up and down
-map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
-map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
-map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-
--- Better indent
-map("v", "<", "<gv", { desc = "Indent left" })
-map("v", ">", ">gv", { desc = "Indent right" })
-
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Clear hlsearch" })
 
 -- Save file
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
--- Better paste
-map("v", "p", '"_dP', { desc = "Better paste" })
-
--- Visual mode enhancements
-map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
-map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
-
 -- Center cursor when scrolling
 map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
 map("n", "n", "nzzzv", { desc = "Next search result and center" })
 map("n", "N", "Nzzzv", { desc = "Previous search result and center" })
+
+-- Select all text (equivalent to ggVG)
+map("n", "<C-a>", "ggVG", { desc = "Select all text" })
 
 -- Oil
 map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
